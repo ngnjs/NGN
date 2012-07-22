0e909137a9a66fea0e3080c2a49f675ff2d4102f
Corey Butler <corey@coreybutler.com>
2012-07-22 09:50:27 -0500

    Refactored examples

--- examples/webserver/server.js
+++ examples/webserver/server.js
@@ -1,13 +1,13 @@
 require('../../');
 
-var web = new NGN.http.Server({
+var web = new NGN.web.Server({
 	port: 8181,
 	routes: __dirname+'/routes'
 });
 
 require('colors');
 web.on('xxx',function(www){
-	console.log('Heard a status:'.green+www.code);
+	console.log('Heard a status: '.green+www.code.toString().green.bold);
 })
 web.on('stop',function(){
 	console.log('stopped'.red);
@@ -15,4 +15,16 @@ web.on('stop',function(){
 web.on('start',function(){
 	console.log('started'.magenta);
 	//web.stop();
+
+	var c = new NGN.web.Client();
+
+	c.on('beforeget',function(){
+		console.log('before get'.magenta);
+	});
+	
+	c.GET('http://localhost:'+web.port,function(a,b,c){
+		console.log(c);
+	});
+
 });
+
