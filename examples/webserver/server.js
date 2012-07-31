0b8a94f4ea247a602fa5e7651e3b15775e33325b
Corey Butler <corey@coreybutler.com>
2012-07-31 12:24:55 -0500

    Docs

--- examples/webserver/server.js
+++ examples/webserver/server.js
@@ -2,16 +2,22 @@ require('../../');
 
 var web = new NGN.web.Server({
 	port: 8181,
-	routes: __dirname+'/routes'
+	routes: './routes'
 });
 
 require('colors');
+
+// Display a message on any status code.
 web.on('xxx',function(www){
 	console.log('Heard a status: '.green+www.code.toString().green.bold);
 })
+
+// Show the server stopped.
 web.on('stop',function(){
 	console.log('stopped'.red);
 });
+
+// Show the server started 
 web.on('start',function(){
 	console.log('started'.magenta);
 	//web.stop();
