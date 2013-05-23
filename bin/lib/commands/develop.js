875d6530b65a951109f52c2713659b8db247de11
Corey Butler <corey@coreybutler.com>
2013-05-23 17:22:17 -0500

    Added initial developer logview tool

--- bin/lib/commands/develop.js
+++ bin/lib/commands/develop.js
@@ -1,37 +1,184 @@
-var util = require('ngn-util'),
-    cli = require('optimist'),
+var cli = require('optimist'),
     p = require('path'),
+    u = require('util'),
     fs = require('fs'),
+    http = require('http'),
+    sockjs = require('sockjs'),
     child_process = require('child_process'),
     spawn = child_process.spawn,
     fork = child_process.fork,
-    procs = {};
+    procs = {},
+    history = [],
+    ip = '127.0.0.1';
 
 require('colors');
 
+// Basic CLI
 var argv = cli
-		  .usage('Usage: ngn keepalive <file>')
+		  .usage('Usage: ngn develop <file> [--port 4444]')
+		  .describe('port','Port number for realtime logging service.')
+		  .alias('p','port')
+		  .default('p',4444)
 		  .argv,
 		files = argv._.filter(function(value){return value !== 'develop'}),
 		myPID = process.pid;
 
-// Cleanup before exiting
-process.on('exit',function(){
-  if (process.pid == myPID) {
-    Object.keys(procs).forEach(function(file){
-      console.log(('Closed development process #'+procs[file].pid).red.bold);
-      process.kill(procs[file].pid);
-      fs.exists(procs[file].out,function(exists){
-        if (exists){
-          fs.unlinkSync(procs[file].out);
-        }
-      });
-    });
-    process.kill(monitor.pid);
-    process.exit();
+// Create a log viewer
+var connected = false,
+    connection = {},
+    unhook = function(){},
+    ws = sockjs.createServer();
+
+// History Management
+var addToHistory = function(data,src){
+  history.push((src !== undefined ? src+'~':'')+data);
+  if (history.length > 10){
+    history.pop();
+  }
+};
+
+// Broadcast data to all connected sockets
+var broadcast = function(data,src){
+  if (data.indexOf('GET') === 0 || data.indexOf('POST') === 0){
+    return;
+  }
+  addToHistory(data,src);
+  for (var id in connection){
+    connection[id].write((src !== undefined ? src+'~':'')+data);
+  }
+}
+
+// Connection
+ws.on('connection', function(conn) {
+
+  // Save Channel
+  connection[conn.id] = conn;
+
+  // Send metadata
+  var s = [];
+  for (var i in procs){
+    s.push(procs[i].pid+'~'+i);
+  }
+  s.splice(0,0,'metadata');
+  conn.write(s.join().toString());
+
+  // Send History
+  for (var i=0; i < history.length; i++){
+    conn.write(history[i]);
+  }
+
+  conn.on('close', function() {
+    delete connection[conn.id];
+    console.log('Log Viewer Closed.')
+  });
+});
+
+// Standard Output Hook
+var hook = function(callback) {
+  var old_write = process.stdout.write,
+      err_write = process.stderr.write;
+
+  process.stdout.write = (function(write) {
+    return function(string, encoding, fd) {
+      write.apply(process.stdout, arguments)
+      callback(string, encoding, fd)
+    }
+  })(process.stdout.write);
+
+  console.log = function (data) {
+    broadcast(data);
+    if (argv.debug){
+      u.debug(data);
+    }
+  };
+
+  console.error = function (data) {
+    broadcast(data);
+    if (argv.debug){
+      u.debug(data);
+    }
+  };
+
+  console.warn = function (data) {
+    broadcast(data);
+    if (argv.debug){
+      u.debug(data);
+    }
+  };
+
+  return function() {
+    process.stdout.write = old_write;
+  //  process.stderr.write = err_write;
+  }
+}
+
+// Get the local IP Address
+var nics = require('os').networkInterfaces();
+for(var adapter in nics){
+  for (var i=0;i<nics[adapter].length; i++){
+    var nic = nics[adapter][i];
+    if (nic.family == 'IPv4'){
+      ip = nic.address;
+      break;
+    }
+  }
+  if (ip !== '127.0.0.1'){
+    break;
+  }
+};
+
+// Web Server
+var www = http.createServer(function (req, res) {
+// Static Content
+var html = fs.readFileSync(p.join(__dirname,'..','..','utilities','logviewer','index.html')),
+    css = fs.readFileSync(p.join(__dirname,'..','..','utilities','logviewer','main.css'));
+  switch(req.url.toLowerCase()){
+    case '/main.css':
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      res.end(css);
+      return;
+    default:
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(html);
+      return;
   }
 });
-console.log('#',myPID);
+
+www.addListener('upgrade', function(req,res){
+  res.end();
+});
+
+ws.installHandlers(www, {prefix:'/logviewer'});
+www.listen(argv.p, '0.0.0.0',function(){
+  console.log(('Log Viewer: http://'+ip+':'+argv.p).cyan.bold);
+  unhook = hook(function(string, encoding, fd) {
+    if (argv.debug){
+      require('util').debug(string);
+    }
+    broadcast(string);
+  });
+});
+
+
+// Cleanup before exiting
+var closeAll = function(){
+  Object.keys(procs).forEach(function(file){
+    broadcast(('Closed development process #'+procs[file].pid).red.bold);
+    try {
+      procs[file].child.kill();
+    } catch(e) {
+      require('util').debug('Process '+procs[file].pid+' not found.');
+    }
+    if (fs.existsSync(procs[file].out)){
+      fs.unlinkSync(procs[file].out);
+    };
+  });
+};
+
+process.on('exit',closeAll);
+process.on("SIGINT", closeAll);
+process.on("SIGTERM", closeAll);
+//process.on("uncaughtException", closeAll);
 
 var cwd = p.dirname(p.resolve(argv.develop));
 var createMonitoredProcess = function(file,output) {
@@ -43,15 +190,28 @@ var createMonitoredProcess = function(file,output) {
     {
       cwd: cwd,
       env: process.env,
-      stdio: ['ignore',out,out],
+      //stdio: ['ignore',sout,out],
       detached: true
     });
 
-  // Unreference the parent
-  child.unref();
+  child.stdout.on('data',function(chunk){
+    broadcast(child.pid+'~'+chunk.toString());
+    if (argv.debug){
+      u.debug(chunk);
+    }
+  });
+  child.stderr.on('data',function(chunk){
+    broadcast('ERROR~'+child.pid+'~'+chunk.toString());
+    if (argv.debug){
+      u.debug(chunk);
+    }
+  });
 
   // Track the process ID and log file for easy shutdown
-  procs[file] = {pid:child.pid,out:output};
+  procs[file] = {child:child,out:output,pid:child.pid};
+
+  // Unreference the parent
+  //child.unref();
 
   return child;
 };
@@ -65,13 +225,12 @@ var monitor = fork(p.join(p.dirname(process.mainModule.filename),'lib','workers'
 monitor.on('message',function(data){
   switch(data.action){
     case "deleted":
-      process.kill(procs[data.file].pid);
+      procs[data.file].child.kill();
       console.log(('Deleted '+data.file).bold.red);
       break;
     case "modified":
-      process.kill(procs[data.file].pid);
-      var c = createMonitoredProcess(data.file,procs[data.file].out);
-      console.log(('Rebooted '+data.file.bold+' as process #'+c.pid.toString().bold).cyan);
+      procs[data.file].child.kill();
+      console.log(('Rebooted '+data.file.bold+' as process #'+createMonitoredProcess(data.file,procs[data.file].out).pid.toString().bold).cyan);
       break;
     default:
       break;
@@ -81,16 +240,20 @@ monitor.on('message',function(data){
 // Initialize the processes
 files.forEach(function(file){
   var nm = (p.resolve(file).replace(p.extname(file),'')+'-out.log').replace(/\/|\\|\:/g,'-').replace(/(\-)\1/gi,'-'),
-      outfile = p.join(util.tempDir,nm),
+      outfile = p.join(require('os').tmpdir(),nm),
       currfile = p.resolve(file);
-      child = createMonitoredProcess(currfile,outfile);
 
-  // Tell the worker to monitor the file
-  monitor.send(currfile);
+  if (!fs.existsSync(currfile)){
+    console.log((currfile+' not found.').red.bold);
+  } else {
+    var child = createMonitoredProcess(currfile,outfile);
 
-  // Console status update
-  console.log(('Developing '.bold+file.bold+' (process #'+child.pid+')').green);
+    // Tell the worker to monitor the file
+    monitor.send(currfile);
 
+    // Console status update
+    console.log(('Tracking '.bold+file.bold+' (process #'+child.pid+')').green);
+  }
 });
 
 // Use TTY and keypress module to maintain an open process
