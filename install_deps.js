bdae2a61fb12055b17da883c60618af9756671a7
Corey Butler <corey@coreybutler.com>
2013-06-07 20:59:51 -0500

    Attempting install update

--- install_deps.js
+++ install_deps.js
@@ -1,20 +1,30 @@
 var os = require('os'),
     p = require('path'),
-    exec = require('child_process').exec;
+    exec = require('child_process').exec,
+    pkg = '';
 
 console.log('Installing background service utilities...');
-exec('npm config get prefix',function(err,out){
-  var path = out,
-      opts = {cwd:p.join(out,'node_modules','ngn')};
-  switch (os.platform().toLowerCase()){
-    case 'win32':
-      exec('npm install node-windows',opts,function(){});
-      break;
-    case 'darwin':
-      exec('npm install node-mac',opts,function(){});
-      break;
-    case 'linux':
-      exec('npm install node-linux',opts,function(){});
-      break;
-  }
+
+var link = function(){
+
+};
+
+switch (os.platform().toLowerCase()){
+  case 'win32':
+    pkg = 'node-windows';
+    break;
+  case 'darwin':
+    pkg = 'node-mac';
+    break;
+  case 'linux':
+    pkg = 'node-linux';
+    break;
+}
+
+exec('npm install -g '+pkg,function(){
+  exec('npm config get prefix',function(err,prefix){
+    require('fs').symlink(p.join(out,'node_modules',pkg),p.join(out,'node_modules','ngn','node_modules',pkg,'dir',function(){
+      console.log('Daemon functionality mapped to NGN.');
+    }));
+  });
 });
