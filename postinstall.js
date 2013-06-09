f6afe200426d26c87b4657f30e300a724daf864d
Corey Butler <corey@coreybutler.com>
2013-06-09 17:51:38 -0500

    debugging

--- postinstall.js
+++ postinstall.js
@@ -18,8 +18,11 @@ switch (os.platform().toLowerCase()){
 }
 
 exec('npm config get prefix',function(err,prefix){
+  console.log('npm global prefix found at '+prefix);
   exec('npm link',{cwd:p.join(prefix,'node_modules',pkg)},function(){
+    console.log(pkg+' link created from cwd: '+p.join(prefix,'node_modules',pkg));
     exec('npm link '+pkg,{cwd:p.join(prefix,'node_modules','ngn')},function(){
+      console.log('Link for '+pkg+' created in '+p.join(prefix,'node_modules','ngn'));
       console.log('Daemon functionality mapped to NGN.');
     });
   });
