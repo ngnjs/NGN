05d10631141caefb8e338a03bb289e8c42a3b900
coreybutler <corey@coreybutler.com>
2013-09-07 21:13:42 -0500

    Removed support for ngn add * shortcut to avoid CLI parsing issues

--- bin/commands/add.js
+++ bin/commands/add.js
@@ -39,7 +39,7 @@ if (available.modules[mod] !== undefined){
   available.groups[mod].forEach(function(m){
     install(m);
   });
-} else if (['all','*'].indexOf(mod.toString().trim().toLowerCase()) >= 0){
+} else if (['all'/*,'*'*/].indexOf(mod.toString().trim().toLowerCase()) >= 0){
   console.log(('Installing every add-on ('+Object.keys(available.modules).length.toString()+' total). This may take a few moments.').cyan.bold);
   for (var m in available.modules){
     install(m);
