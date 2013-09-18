ee7ef3d4f7cb64ca0cead17cbcf69eae2c43fbb4
coreybutler <corey@coreybutler.com>
2013-09-18 18:06:26 -0500

    Added -v support and verbose module list. Closes #35.

--- bin/commands/version.js
+++ bin/commands/version.js
@@ -1,3 +1,15 @@
-var pkg = require(require('path').join(__dirname,'..','..','package.json'));
+var p = require('path'),
+  pkg = require(p.join(__dirname,'..','..','package.json')),
+  fs = require('fs');
 require('colors');
-console.log('NGN v'.bold+pkg.version.bold);
+
+console.log((' + NGN v'+pkg.version).bold);
+for (var mod in pkg.ngn.modules){
+  var path = p.join(__dirname,'../../../',mod);
+  if (fs.existsSync(path)){
+    var mpkg = require(p.join(path,'package.json'));
+    console.log((' + '+mod.cyan+' v'+mpkg.version).bold);
+  } else {
+    console.log(' - '+mod+' (Not Installed)'.red);
+  }
+}
