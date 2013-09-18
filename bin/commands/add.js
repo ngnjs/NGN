5ee400b2ef6ef43c24a4b9e341281607c4a593df
coreybutler <corey@coreybutler.com>
2013-09-18 15:01:23 -0500

    Added feedback for module installation. Fixes #38

--- bin/commands/add.js
+++ bin/commands/add.js
@@ -5,7 +5,8 @@ var cli = require('optimist'),
     fs = require('fs'),
     exec = require('child_process').exec,
     Seq = require('seq'),
-    read = require('read');
+    read = require('read'),
+    events = require('events');
 
 var available = require(p.join(process.mainModule.paths[0],'..','..','package.json')).ngn;
 
@@ -13,23 +14,46 @@ var available = require(p.join(process.mainModule.paths[0],'..','..','package.js
 var argv = cli
 		  .usage('Usage: ngn add <module or group>')
 		  .argv,
-		mod = argv._.filter(function(value){return value !== 'add'})[0];
+		mod = argv._.filter(function(value){return value !== 'add';})[0];
 
 // Global Package Installer
 var install = function(ngnpkg){
-  exec('npm install -g '+ngnpkg+' --json --loglevel=silent',function(err,stdout,stderr){
-    try {
-      // Handle edge case when gyp output screws up npm json format
-      if (stdout.substr(0,1) !== '['){
-        stdout = stdout.substr(stdout.indexOf('['),stdout.length);
+  var evt = new events.EventEmitter();
+
+  evt.on('install',function(pkg){
+    exec('npm install -g '+pkg+' --json --loglevel=silent',function(err,stdout,stderr){
+      try {
+        // Handle edge case when gyp output screws up npm json format
+        if (stdout.substr(0,1) !== '['){
+          stdout = stdout.substr(stdout.indexOf('['),stdout.length);
+        }
+        var out = JSON.parse(stdout)[0];
+        console.log((out.name.toString()+' support added --> '+' v'+out.version).green.bold);
+      } catch (e) {
+        console.log('Module installed, but may have errors:'.yellow.bold);
+        console.log(e.message.toString().yellow);
       }
-      var out = JSON.parse(stdout)[0];
-      console.log((out.name.toString()+' support added --> '+' v'+out.version).green.bold);
-    } catch (e) {
-      console.log('Module installed, but may have errors:'.yellow.bold);
-      console.log(e.message.toString().yellow);
-    }
+    });
   });
+
+  if (fs.existsSync(p.join(__dirname,'..','..','..',ngnpkg))){
+    var semver = require('semver'),
+      currv = require(p.join(__dirname,'..','..','..',ngnpkg,'package.json')).version;
+
+    console.log((ngnpkg+' version '+currv+' is already installed.\n').yellow+('Checking for updates...').yellow.bold);
+    exec('npm show '+ngnpkg+' version --silent',function(err,stdo,stde){
+      if (err) throw err;
+      if (semver.lt(currv,stdo)){
+        console.log((('A new version ('+stdo.toString().trim()).green.bold+') is available. Update in progress...'.green.bold));
+        evt.emit('install',ngnpkg);
+      } else {
+        console.log(currv.toString().yellow.bold+' is the latest version.'.yellow);
+        return;
+      }
+    });
+  } else {
+    evt.emit('install',ngnpkg);
+  }
 };
 
 // Check first for a module or group and install/warn accordingly
@@ -45,5 +69,5 @@ if (available.modules[mod] !== undefined){
     install(m);
   };
 } else {
-  throw 'No module or group called \"'+mod+'\" is available.';
+  console.log(('No module or group called \"'+mod+'\" is available.').red.bold);
 }
