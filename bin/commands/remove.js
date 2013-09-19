98e507f02034cbb7c5eaf10eca920fc54088d576
coreybutler <corey@coreybutler.com>
2013-09-18 22:08:40 -0500

    Added user permission check for remove functionality. Addresses part of issue #36.

--- bin/commands/remove.js
+++ bin/commands/remove.js
@@ -1,23 +1,55 @@
 require('colors');
 var cli = require('optimist'),
     p = require('path'),
-    u = require('util'),
     fs = require('fs'),
-    exec = require('child_process').exec,
-    Seq = require('seq'),
-    read = require('read');
+    exec = require('child_process').exec;
 
 var available = require(p.join(process.mainModule.paths[0],'..','..','package.json')).ngn;
 
+var rmDir = function(path) {
+  var files = [];
+  if( fs.existsSync(path) ) {
+    files = fs.readdirSync(path);
+    files.forEach(function(file,index){
+      var curPath = path + "/" + file;
+      if(fs.statSync(curPath).isDirectory()) { // recurse
+        rmDir(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
+
 // Basic CLI
 var argv = cli
-		  .usage('Usage: ngn remove <module or group>')
-		  .argv,
-		mod = argv._.filter(function(value){return value !== 'remove'})[0];
+    .usage('Usage: ngn remove <module or group>')
+    .argv,
+  mod = argv._.filter(function(value){return value !== 'remove'})[0];
 
 // Global Package Installer
 var uninstall = function(ngnpkg){
-  exec('npm uninstall -g '+ngnpkg+' --json --loglevel=silent',function(err,stdout,stderr){
+  var path = p.join(__dirname,'..','..','..',ngnpkg);
+  if (fs.existsSync(path)){
+    var v = require(p.join(path,'package.json')).version;
+    try {
+      rmDir(path);
+      console.log((ngnpkg+' support removed --> '+' v'+v).red.bold);
+    } catch (e) {
+      if (e.message.toString().toLowerCase().indexOf('permission') >= 0){
+        console.log('You have insufficient permission to remove modules.'.red.bold);
+        if (require('os').platform() == 'win32'){
+          console.log('\nPlease run this command with an admin account.'.yellow.bold);
+        } else {
+          console.log('\nPlease run this command as root (or sudo).'.yellow.bold);
+        }
+      }
+    }
+  } else {
+    console.log((ngnpkg+' support not found/installed. Skipped.').yellow);
+  }
+  /*exec('npm uninstall -g '+ngnpkg+' --json --loglevel=silent',function(err,stdout,stderr){
     if (stdout.toString().trim().length == 0){
       console.log((ngnpkg+' support not found/installed. Skipped.').yellow);
       return;
@@ -29,7 +61,7 @@ var uninstall = function(ngnpkg){
       console.log('Module removed, but there were errors:'.yellow.bold);
       console.log(e.message.toString().yellow);
     }
-  });
+  });*/
 };
 
 // Check first for a module or group and install/warn accordingly
