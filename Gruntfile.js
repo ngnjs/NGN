84e664ea032b65c93ee1d6528fc530a10f71d377
coreybutler <=corey@coreybutler.cmom>
2014-03-06 15:25:39 -0600

    Updated documentation generator

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,8 @@
 module.exports = function(grunt) {
 
   var cfg = {};
+	var path = require('path'),
+			fs = require('fs');
 
   Object.defineProperties(cfg,{
     _ngn: {
@@ -20,24 +22,57 @@ module.exports = function(grunt) {
 		output: {
 			enumerable: false,
 			get: function(){
-				return require('path').resolve('../../Website/docs.nodengn.com/ngn-'+cfg.ngn.version.replace(/\./g,'-'));
+				return path.resolve('../../Website/docs.nodengn.com/ngn-'+cfg.ngn.version.replace(/\./g,'-'));
 			}
 		},
 		input:{
 			enumerable: false,
 			value: {
-				docs: require('path').resolve('../../Documentation'),
-				code: require('path').resolve('../../Library')
+				docs: path.resolve('../../Documentation'),
+				code: path.resolve('../../Library')
 			}
     },
+		node_modules:{
+			enumerable: false,
+			writable: true,
+			configurable: false,
+			value: null
+		},
+		nodemodules:{
+			enumerable: true,
+			get: function(){
+				if(this.node_modules === null){
+					this.node_modules = [];
+					var fs = require('fs'), me = this;
+					(fs.readdirSync(this.input.code)||[]).forEach(function(dir){
+						if (fs.statSync(require('path').dirname(dir)).isDirectory()){
+							var pkg = require('path').join(dir,'package.json');
+							if (fs.existsSync(pkg)){
+								var p = require(pkg);
+								if (p.hasOwnProperty('dependencies')){
+									me.node_modules = me.node_modules.concat(Object.keys(p.dependencies));
+								}
+								if (p.hasOwnProperty('devDependencies')){
+									me.node_modules = me.node_modules.concat(Object.keys(p.devDependencies));
+								}
+							}
+						}
+					});
+					this.node_modules = this.node_modules.filter(function(el,i,a){
+						return a.indexOf(el) === i;
+					});
+				}
+				return this.node_modules;
+			}
+		},
 		productDocs: {
 			enumerable: false,
 			get: function(){
 				var p = [];
-				require('fs').readdirSync(require('path').join(this.output,'..')).forEach(function(dir,i,a){
-					var d = require('path').basename(dir),
-							v = d.split('-').splice(0,1).join('.');
-					if (v !== cfg.ngn.version) {
+				fs.readdirSync(path.join(this.output,'..')).forEach(function(dir,i,a){
+					var d = path.basename(dir),
+							v = d.split('-').slice(1,d.split('-').length).join('.');
+					if (v !== cfg.ngn.version && d !== 'latest') {
 						var	nm = d.split('-')[0].replace('ngn','NGN')+' '+v;
 						p.push("{text: '"+nm+"', href: './"+d+"'}"+(i<a.length?',':''));
 					}
@@ -52,8 +87,10 @@ module.exports = function(grunt) {
 				var exclusions = [];
 				exclusions = wrench.readdirSyncRecursive(this.input.code).filter(function(d){
 					// Skip anything that isn't in the list of ignored directories
-					return (d.match(/node_modules|\.git/gi)||[]).length === 1 
-						&& ['node_modules','.git'].indexOf(require('path').basename(d).toLowerCase()) >= 0;
+					return (d.match(/node_modules|\.git|test/gi)||[]).length === 1 
+						&& ['node_modules','.git','test'].indexOf(path.basename(d).toLowerCase()) >= 0;
+				}).map(function(el){
+					return path.resolve('../'+el);
 				});
 				return exclusions;
 			}
@@ -147,13 +184,13 @@ module.exports = function(grunt) {
           './lib',
           cfg.input.docs+'/node',
           cfg.input.code+'/ngn-idk-core',
-          cfg.input.code+'ngn-idk-http-web',
-          cfg.input.code+'ngn-idk-http-data',
-          cfg.input.code+'ngn-idk-http-proxy',
-          cfg.input.code+'ngn-idk-http-rpc',
-          cfg.input.code+'ngn-idk-http-tcp',
-          cfg.input.code+'ngn-idk-mail',
-          cfg.input.code+'ngn-sdk'
+          cfg.input.code+'/ngn-idk-http-web',
+          cfg.input.code+'/ngn-idk-http-data',
+          cfg.input.code+'/ngn-idk-http-proxy',
+          cfg.input.code+'/ngn-idk-rpc',
+          cfg.input.code+'/ngn-idk-tcp',
+          cfg.input.code+'/ngn-idk-mail',
+          cfg.input.code+'/ngn-sdk'
         ],
 
         // docs output dir
@@ -170,7 +207,7 @@ module.exports = function(grunt) {
           //"meta-tags": "docs/custom/tags",
           "builtin-classes": true,
           "warnings": [],
-          "external": ['XMLHttpRequest'],
+          "external": ['XMLHttpRequest'].concat(cfg.nodemodules),
 					"exclude": cfg.docexclusions,
 					"body-html": cfg.productDocs
         }
@@ -191,23 +228,36 @@ module.exports = function(grunt) {
 	
 	grunt.task.registerTask('jsduckcheck', 'Make sure the doc directories are available for output.', function(){
 		var done = this.async();
-		console.log('>>>>>>>>>>>>>>>>>>>>>>',cfg.docexclusions);
-		require('fs').exists(cfg.output,function(exists){
+		var unlink = function(){
+			fs.exists(path.join(path.dirname(cfg.output),'latest'),function(exists){
+				if (exists){
+					fs.unlink(path.join(path.dirname(cfg.output),'latest'),done);
+				} else {
+					done();
+				}
+			});
+		}
+		
+		fs.exists(cfg.output,function(exists){
 			if (!exists){
-				require('fs').mkdir(cfg.output,done);
+				fs.mkdir(cfg.output,unlink);
 			} else {
-				done();
+				unlink();
 			}
 		});
+	});
+	
+	grunt.task.registerTask('linklatestdoc','Link the latest docs', function(){
+		fs.symlink(cfg.output,path.join(path.dirname(cfg.output),'latest'),'dir',this.async);
 	});
 
   grunt.task.registerTask('check', 'Check the build', function() {
     var mods = require('./package.json').ngn.modules;
     for (var dir in mods){
       var done = this.async();
-      grunt.log.writeln(require('path').join(process.cwd(),dir));
+      grunt.log.writeln(path.join(process.cwd(),dir));
       require('child_process').exec("git status",{
-        cwd: require('path').join(process.cwd(),dir),
+        cwd: path.join(process.cwd(),dir),
         env: process.env
       },function(err,result){
         console.log(err);
@@ -225,5 +275,5 @@ module.exports = function(grunt) {
 
   // Default task.
   grunt.registerTask('default', 'jshint');
-  grunt.registerTask('docs', ['jsduckcheck','jsduck','copy:jsduckassets']);
+  grunt.registerTask('docs', ['jsduckcheck','jsduck','copy:jsduckassets','linklatestdoc']);
 };
