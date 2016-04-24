39c7606b91aacfd1a65a8730bb4981e578a04316
Corey Butler <corey@coreybutler.com>
2016-04-23 19:00:39 -0500

    Added logging middleware. Version bump.

--- test/05-log.js
+++ test/05-log.js
@@ -12,6 +12,9 @@ test('Custom Logging', function (t) {
   t.ok(typeof NGN.Log.createMethod === 'function', 'Method for decorating console exists.')
   t.ok(typeof NGN.Log.write === 'function', 'Method for writing to stdout exists.')
   t.ok(typeof NGN.Log.on === 'function', 'NGN.Log object can handle events.')
+  t.ok(typeof NGN.Log.write === 'function', 'NGN.Log.write can write to stdout.')
+  t.ok(typeof NGN.Log.use === 'function', 'NGN.Log.use is a valid function.')
+  t.ok(typeof NGN.Log.unuse === 'function', 'NGN.Log.unuse is a valid function.')
 
   // Functional Checks
   NGN.Log.once('logevent', function (evt) {
@@ -32,19 +35,34 @@ test('Custom Logging', function (t) {
 
       t.ok(console.critical !== undefined, 'Successfully added a custom logging method (critical).')
 
-      NGN.Log.disable()
-//      NGN.Log.level = ['critical', 'error']
-//
-//      NGN.Log.once('info', function () {
-//        t.fail('Failed to filter properly.')
-//      })
-//
-//      NGN.Log.once('critical', function () {
-//        NGN.Log.disable()
-//      })
-//
-//      console.info('generic info')
-//      console.critical('crtical test')
+      let middleware = function (input) {
+        input.unshift('prefix')
+        return input
+      }
+
+      NGN.Log.use('log', middleware)
+
+      NGN.Log.once('logevent', function (evt) {
+        if (evt.name === 'log') {
+          NGN.Log.unuse('log', middleware)
+          t.ok(evt.data.split(' ')[0] === 'prefix', 'Middleware successfully modified log output.')
+          t.ok(!NGN.Log.middleware.hasOwnProperty('log'), 'Middleware removed successfully.')
+          NGN.Log.disable()
+        }
+      })
+      console.log('change log')
+    //      NGN.Log.level = ['critical', 'error']
+    //
+    //      NGN.Log.once('info', function () {
+    //        t.fail('Failed to filter properly.')
+    //      })
+    //
+    //      NGN.Log.once('critical', function () {
+    //        NGN.Log.disable()
+    //      })
+    //
+    //      console.info('generic info')
+    //      console.critical('crtical test')
     })
 
     NGN.Log.once('disabled', function () {
